e901a28ba5267eeb1fb9c64041065518a13eed9f
InvictusKhan <fasihkhan8352@gmail.com>
2026-07-13 12:09:40 +0400

    Practice till server

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
+// import http from "http";
+
+// const server = http.createServer(function(req, res){
+
+//     res.end(`Hello World`);
+// });
+
+// server.listen(3000, function(){
+//     console.log("The server is running on port 3000")
+// });
+
+
+
+
+// import http from "http";
+
+// const server = http.createServer(function(req, res){
+//     res.end(`Server is live`);
+// });
+
+// server.listen(9000, function(){
+//     console.log("Server is live");
+// });
+
+
 import http from "http";
 
 const server = http.createServer(function(req, res){
-
-    res.end(`Hello World`);
+    
+    res.end("Your Form has been submitted successfully✅");
 });
 
-server.listen(3000, function(){
-    console.log("The server is running on port 3000")
-});
+server.listen(5000, function(){
+    console.log("Server is up!")
+})
