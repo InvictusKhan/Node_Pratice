9bed687c72101bf2b3a9f1334cbc4a9f97b3b3eb
InvictusKhan <fasihkhan8352@gmail.com>
2026-07-14 10:28:04 +0400

    Node Practice, routing and ENV req

--- server.js
+++ server.js
@@ -23,13 +23,51 @@
 // });
 
 
+// import http from "http";
+
+// const server = http.createServer(function(req, res){
+    
+//     res.end("Your Form has been submitted successfully✅");
+// });
+
+// server.listen(5000, function(){
+//     console.log("Server is up!")
+// })
+
+
+import { error } from "console";
 import http from "http";
+const PORT = process.env.PORT;
+
+
+
 
 const server = http.createServer(function(req, res){
-    
-    res.end("Your Form has been submitted successfully✅");
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                console.log(req.method);
+                console.log(req.url);
+                res.end("Homepage");
+            } else if (req.url === '/about') {
+                console.log(req.method);
+                console.log(req.url);
+                res.end("About Page");
+            } else {
+                console.log(req.method);
+                console.log(req.url);
+                res.end('Page Not Found');
+            }
+        } else {
+            throw new Error("Method not Allowed");
+        }
+    } catch (err) {
+        console.log(req.method);
+        console.log(req.url);
+        console.error(err);
+        res.end('Server Error');
+    }
 });
-
-server.listen(5000, function(){
-    console.log("Server is up!")
-})
+server.listen(PORT, function(){
+    console.log(`Server is live on ${PORT}`);
+});
